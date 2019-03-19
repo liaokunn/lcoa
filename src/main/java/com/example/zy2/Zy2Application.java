2f97f9c02f9874df5d6c1369fb8f9e223f2ab876
liaokunn <120357131@qq.com>
2019-03-19 17:16:20 +0800

    雷智超提交的

---
+++ src/main/java/com/example/zy2/Zy2Application.java
@@ -0,0 +1,13 @@
+package com.example.zy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zy2Application.class, args);
+    }
+
+}
